ddaf72e2e57515a4e23f48edddb67d4cd79d823c
arshad <arshad@Ubuntu.myguest.virtualbox.org>
2024-08-09 14:41:45 +0530

    new changes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-#app.py
+from flask import Flask
 
-print("This is simple python file")
+app = Flask(__name__)
 
-print("version2")
+@app.route('/')
+def hello():
+    return "This is simple python project"
 
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Ensure it's set to listen on port 5000
--- dockerfile
+++ dockerfile
@@ -4,6 +4,11 @@ FROM python:3.8.10
 
 WORKDIR /app
 
+# Copy the requirements file and install dependencies
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
 COPY . .
 
 CMD ["python3","app.py"]
